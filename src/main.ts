42e353007016877d617cb582cc015a136412da49
Lu-Zhanxing <18330259691@qq.com>
2023-07-29 21:53:21 +0800

    测试husky配置

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-const aa = function () {
-  console.log('dfadf')
+const aa=function(){
+  console.log("你好")
+  console.log("dfadf")
 }
 
 aa()
