90286dfac95b3715ba5506e3550dbe0873616b5b
Lu-Zhanxing <18330259691@qq.com>
2023-09-09 15:47:43 +0800

    已有品牌ts类型

--- src/api/product/trademark/index.ts
+++ src/api/product/trademark/index.ts
@@ -1,10 +1,11 @@
-import request from "@/utils/request";
+import request from '@/utils/request'
+import type { TradeMarkResponseData } from './type'
 
-enum API{
-    TRADEMARK_URL = '/admin/product/baseTrademark/'
+enum API {
+  TRADEMARK_URL = '/admin/product/baseTrademark/',
 }
 
 // 获取已有品牌的数据
-export const reqHasTradeMark = (page:number,limit:number) =>{
-    return request.get<any,any>(API.TRADEMARK_URL + `${page}/${limit}`)
+export const reqHasTradeMark = (page: number, limit: number) => {
+  return request.get<any, TradeMarkResponseData>(API.TRADEMARK_URL + `${page}/${limit}`)
 }
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,18 +1,33 @@
 <template>
   <el-card class="box-card">
     <el-button type="primary" icon="Plus">添加</el-button>
-    <el-table border style="margin:10px 0" :data="trademarArr">
-      <el-table-column label="序号" width="80" type="index" align="center"></el-table-column>
+    <el-table border style="margin: 10px 0" :data="trademarArr">
+      <el-table-column
+        label="序号"
+        width="80"
+        type="index"
+        align="center"
+      ></el-table-column>
       <el-table-column label="名称" prop="tmName"></el-table-column>
       <el-table-column label="图片">
-        <template #="{row,$index}">
-          <img :src="row.logoUrl" style="width: 60px;height: 60px;">
+        <template #="{ row, $index }">
+          <img :src="row.logoUrl" style="width: 60px; height: 60px" />
         </template>
       </el-table-column>
       <el-table-column label="操作">
-        <template #="{row,$index}">
-          <el-button type="primary" size="small" @click="" icon="Edit"></el-button>
-          <el-button type="danger" size="small" @click="" icon="Delete"></el-button>
+        <template #="{ row, $index }">
+          <el-button
+            type="primary"
+            size="small"
+            @click=""
+            icon="Edit"
+          ></el-button>
+          <el-button
+            type="danger"
+            size="small"
+            @click=""
+            icon="Delete"
+          ></el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -20,7 +35,7 @@
       v-model:current-page="currentPage"
       v-model:page-size="pageSize"
       :page-sizes="[3, 5, 10]"
-      background=true
+      background="true"
       layout=" prev, pager, next, jumper, ->, sizes, total"
       :total="total"
     />
@@ -28,22 +43,23 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted } from 'vue'
 import { reqHasTradeMark } from '@/api/product/trademark/index'
+import type { Records, TradeMarkResponseData } from '@/api/product/trademark/type'
 const currentPage = ref<number>(1)
 const pageSize = ref<number>(3)
 const total = ref<number>(0)
-const trademarArr = ref<any>([])
+const trademarArr = ref<Records>([])
 
-const getHasTrademark = async() =>{
-  const result = await reqHasTradeMark(currentPage.value,pageSize.value)
-  if(result.code == 200){
+const getHasTrademark = async () => {
+  const result:TradeMarkResponseData = await reqHasTradeMark(currentPage.value, pageSize.value)
+  if (result.code == 200) {
     total.value = result.data.total
-    trademarArr.value = result.data.records    
+    trademarArr.value = result.data.records
   }
 }
 
-onMounted(()=>{
+onMounted(() => {
   getHasTrademark()
 })
 </script>
